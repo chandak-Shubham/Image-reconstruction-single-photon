ebf9547b8a708b989eafd728359522b787b9ced1
Shubham Chandak <shubhamchandak200@gmail.com>
2026-04-19 18:46:04 +0530

    Update train.py

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
 from torchvision import models
 
-from modified.dataloader.dataset_loader import get_train_scenes, SPCDataset
-from modified.models.attention_resunet import AttentionResUNet
+from dataloader.dataset_loader import get_train_scenes, SPCDataset
+from model.attention_resunet import AttentionResUNet
 
 DATASET = "Data"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
